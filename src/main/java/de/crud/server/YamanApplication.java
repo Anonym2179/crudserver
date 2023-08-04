5dc9e441f34f208b05b07c7989c04e005a3b0784
emin <emin@duweisst.de>
2023-08-05 01:00:02 +0200

    blabla

---
+++ src/main/java/de/crud/server/YamanApplication.java
@@ -0,0 +1,13 @@
+package de.crud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/crud/server/YamanApplicationTests.java
@@ -0,0 +1,13 @@
+package de.crud.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
