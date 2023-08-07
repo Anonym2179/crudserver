76ff7df026c1d4c614c27b4b4ec26bc839f03a3c
emin <emin@duweisst.de>
2023-08-07 17:52:32 +0200

    updated database

--- database/crud_database.sql
+++ database/crud_database.sql
@@ -1,8 +1,16 @@
-DROP TABLE IF EXISTS 'product';
-
-CREATE TABLE 'product'(
-    'product_id' begint NOT NULL AUTO_INCREMENT,
-    'product_name' varchar(255) DEFAULT NULL,
-    'product_price' varchar(255) NOT NULL,
-    PRIMARY KEY (product_id)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: shop
+-- ------------------------------------------------------
+-- Server version	8.0.33
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `product_id` bigint NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(255) DEFAULT NULL,
+  `product_price` varchar(255) NOT NULL,
+  `product_stock` varchar(255) NOT NULL,
+  `id` int NOT NULL,
+  PRIMARY KEY (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
